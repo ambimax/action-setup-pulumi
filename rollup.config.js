93e041f1bdafe3750b91bb6c02b78dda3ef89897
FaustTobias <tf@ambimax.de>
2020-03-16 10:29:51 +0100

    Fix output folder

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,12 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
+
 export default {
     input: "src/index.ts",
     output: {
         format: "cjs",
-        file: "lib/index.js",
+        file: "dist/index.js",
     },
     plugins: [
         commonjs(),
